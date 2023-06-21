52342d4c56794d5c61d3961398af141ecc9030ea
知念英弥 <s19013@std.it-college.ac.jp>
2023-06-21 18:51:22 +0900

    todoのアイテム単品を管理するコンポーネント

---
+++ src/Components/TodoItem.js
@@ -0,0 +1,18 @@
+export default function TodoItem({item,onCheck}) {
+    const handleChange = () => {
+        onCheck(item)
+    }
+
+    return (
+        <label className="panel-block">
+            <input
+                type="checkbox"
+                checked={item.done}
+                onChange={handleChange}
+            />
+            <span className={item.done ? 'has-text-grey-light':''}>
+                {item.text}
+            </span>
+        </label>
+    )
+}
